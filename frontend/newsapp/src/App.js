799e8d99e1da203a878f291952d09f7ecbd11863
tbharathchandra <t.bharathchandra@gmail.com>
2020-09-03 17:59:43 +0530

    added tv9 hydnews and tv6 and made some changes to UI

--- frontend/newsapp/src/App.js
+++ frontend/newsapp/src/App.js
@@ -19,14 +19,21 @@ class App extends Component{
     this.state = {
       timesdata: [],
       hindudata: [],
-      livedata:[],
+      livedata: [],
+      hydnewsdata: [],
+      tv9data: [],
+      tv6data: [],
     }
+
   };
 
   componentDidMount() {
     var timesd = localStorage.getItem("timesdata");
     var hindud = localStorage.getItem("hindudata");
     var lived = localStorage.getItem("livedata");
+    var hydnewsd = localStorage.getItem("hydnewsdata");
+    var tv9d = localStorage.getItem("tv9data");
+    var tv6d = localStorage.getItem("tv6data");
     if(timesd !== null) {
       var d = JSON.parse(timesd);
       this.setState({timesdata: d});
@@ -38,6 +45,16 @@ class App extends Component{
     if(lived !== null) {
       var l = JSON.parse(lived);
       this.setState({livedata: l});
+    }
+    if(hydnewsd !== null) {
+      var l = JSON.parse(hydnewsd);
+      this.setState({hydnewsdata: l});
+    }if(tv9d !== null) {
+      var l = JSON.parse(tv9d);
+      this.setState({tv9data: l});
+    }if(tv6d !== null) {
+      var l = JSON.parse(tv6d);
+      this.setState({tv6data: l});
     }
     console.log("App-ComponentDidMount");
 
@@ -59,6 +76,24 @@ class App extends Component{
   addLiveData = (authorName, authorTag) => {
     this.setState({livedata: [...this.state.livedata, {authorName: authorName, authorTag: authorTag}]}, () => {
       localStorage.setItem("livedata", JSON.stringify(this.state.livedata));
+    });
+  }
+
+  addHydNewsData = (authorName, authorTag) => {
+    this.setState({hydnewsdata: [...this.state.hydnewsdata, {authorName: authorName, authorTag: authorTag}]}, () => {
+      localStorage.setItem("hydnewsdata", JSON.stringify(this.state.hydnewsdata));
+    });
+  }
+
+  addTv9Data = (authorName, authorTag) => {
+    this.setState({tv9data: [...this.state.tv9data, {authorName: authorName, authorTag: authorTag}]}, () => {
+      localStorage.setItem("tv9data", JSON.stringify(this.state.tv9data));
+    });
+  }
+
+  addTv6Data = (authorName, authorTag) => {
+    this.setState({tv6data: [...this.state.tv6data, {authorName: authorName, authorTag: authorTag}]}, () => {
+      localStorage.setItem("tv6data", JSON.stringify(this.state.tv6data));
     });
   }
 
@@ -94,6 +129,39 @@ class App extends Component{
     }
   }
 
+  deleteHydNewsData = (data) => {
+    var array = [...this.state.hydnewsdata];
+    var index = array.indexOf(data);
+    if(index!==-1) {
+      array.splice(index, 1);
+      this.setState({hydnewsdata: array}, () => {
+        localStorage.setItem("hydnewsdata", JSON.stringify(array));
+      });
+    }
+  }
+
+  deleteTv9Data = (data) => {
+    var array = [...this.state.tv9data];
+    var index = array.indexOf(data);
+    if(index!==-1) {
+      array.splice(index, 1);
+      this.setState({tv9data: array}, () => {
+        localStorage.setItem("tv9data", JSON.stringify(array));
+      });
+    }
+  }
+
+  deleteTv6Data = (data) => {
+    var array = [...this.state.tv6data];
+    var index = array.indexOf(data);
+    if(index!==-1) {
+      array.splice(index, 1);
+      this.setState({tv6data: array}, () => {
+        localStorage.setItem("tv6data", JSON.stringify(array));
+      });
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -104,18 +172,32 @@ class App extends Component{
               <Home timesdata={this.state.timesdata} 
                 hindudata={this.state.hindudata} 
                 livedata={this.state.livedata}
+                hydnewsdata={this.state.hydnewsdata}
+                tv9data={this.state.tv9data}
+                tv6data={this.state.tv6data}
+
                 aTimes={this.addTimesData} 
                 aHindu={this.addHinduData} 
                 aLive={this.addLiveData}
+                aHydNewsData={this.addHydNewsData}
+                atv9Data={this.addTv9Data}
+                atv6Data={this.addTv6Data}
+
                 dtimes={this.deleteTimesData} 
                 dhindu={this.deleteHinduData} 
                 dlive={this.deleteLiveData}
+                dHydNewsData={this.deleteHydNewsData}
+                dTv6Data={this.deleteTv6Data}
+                dTv9Data={this.deleteTv9Data}
               />
             </Route>
             <Route path="/news">
               <News timesdata={this.state.timesdata}
-               hindudata={this.state.hindudata}
-               livedata={this.state.livedata}
+                hindudata={this.state.hindudata}
+                livedata={this.state.livedata}
+                hydnewsdata={this.state.hydnewsdata}
+                tv9data={this.state.tv9data}
+                tv6data={this.state.tv6data}
                />
             </Route>
           </Switch>
--- frontend/newsapp/src/components/Home.js
+++ frontend/newsapp/src/components/Home.js
@@ -9,6 +9,9 @@ const base_url = {
     "times_now": "https://www.timesnownews.com/expert/",
     "the_hindu": "https://www.thehindu.com/profile/author/",
     "livemint": "https://www.livemint.com/Search/Link/Author/",
+    "hyd_news": "http://www.hydnews.net/author/",
+    "tv9": "https://tv9telugu.com/author/",
+    "tv6": "https://www.tv6tnt.com/users/profile/"
 };
 
 class Home extends Component {
@@ -46,6 +49,29 @@ class Home extends Component {
                 alert("URL is not of Livemint, Please enter the right one");
                 return;
             }
+        }else if(this.asource.current.value === 'HydNews') {
+            if(full_url.substring(0, base_url.hyd_news.length)===base_url.hyd_news){
+                authorTag = full_url.substring(base_url.hyd_news.length);
+            }else{
+                alert("URL is not of Hydnews, Please enter the right one");
+                return;
+            }
+        }else if(this.asource.current.value === 'Tv9') {
+            if(full_url.substring(0, base_url.tv9.length)===base_url.tv9){
+                authorTag = full_url.substring(base_url.tv9.length);
+            }else{
+                alert("URL is not of Tv9, Please enter the right one");
+                return;
+            }
+        }else if(this.asource.current.value === 'Tv6') {
+            if(full_url.substring(0, base_url.tv6.length)===base_url.tv6){
+                authorTag = full_url.substring(base_url.tv6.length);
+                // authorTag =  authorTag.replace(" ", "%20");
+
+            }else{
+                alert("URL is not of Tv6, Please enter the right one");
+                return;
+            }
         }
         if(authorTag.charAt(0)==='/') {
             authorTag = authorTag.slice(1);
@@ -75,6 +101,24 @@ class Home extends Component {
                 return; 
             }
             this.props.aLive(authorName, authorTag);
+        }else if(this.asource.current.value === 'HydNews') {
+            if(this.props.hydnewsdata.some((e) => {return JSON.stringify({authorName: authorName, authorTag: authorTag})===JSON.stringify(e)})) {
+                alert("This Entry already exists");
+                return; 
+            }
+            this.props.aHydNewsData(authorName, authorTag);
+        }else if(this.asource.current.value === 'Tv9') {
+            if(this.props.tv9data.some((e) => {return JSON.stringify({authorName: authorName, authorTag: authorTag})===JSON.stringify(e)})) {
+                alert("This Entry already exists");
+                return; 
+            }
+            this.props.atv9Data(authorName, authorTag);
+        }else if(this.asource.current.value === 'Tv6') {
+            if(this.props.tv6data.some((e) => {return JSON.stringify({authorName: authorName, authorTag: authorTag})===JSON.stringify(e)})) {
+                alert("This Entry already exists");
+                return; 
+            }
+            this.props.atv6Data(authorName, authorTag);
         }
         this.name.current.value = "";
         this.atag.current.value = "";
@@ -92,6 +136,15 @@ class Home extends Component {
 
     deleteLive = (Name) => {
         this.props.dlive(Name);
+    }
+    deleteHydnews = (Name) => {
+        this.props.dHydNewsData(Name);
+    }
+    deleteTv9 = (Name) => {
+        this.props.dTv9Data(Name);
+    }
+    deleteTv6 = (Name) => {
+        this.props.dTv6Data(Name);
     }
 
     render() {
@@ -113,6 +166,9 @@ class Home extends Component {
                                         <option>Times Now</option>
                                         <option>The Hindu</option>
                                         <option>Livemint</option>
+                                        <option>HydNews</option>
+                                        <option>Tv9</option>
+                                        <option>Tv6</option>
                                     </Form.Control>
                                     <br />
                                     <Form.Row>
@@ -159,6 +215,33 @@ class Home extends Component {
                                         <span>{data.authorName}</span>
                                     </a>
                                     <a><AiFillCloseSquare className="cross" onClick={() => this.deleteLive(data)}/></a>
+                                </span>
+                            ))}
+                            { this.props.hydnewsdata.length>0 && (<h5 className="form-sub-heading">Hydnews Authors</h5>) }  
+                            {this.props.hydnewsdata.map((data, idx) => (
+                                <span key={idx} className="tagbox-span">
+                                    <a className="tagbox-name" target="_blank" href={base_url.hyd_news+data.authorTag} >
+                                        <span>{data.authorName}</span>
+                                    </a>
+                                    <a><AiFillCloseSquare className="cross" onClick={() => this.deleteHydnews(data)}/></a>
+                                </span>
+                            ))}
+                            { this.props.tv9data.length>0 && (<h5 className="form-sub-heading">Tv9 Authors</h5>) }  
+                            {this.props.tv9data.map((data, idx) => (
+                                <span key={idx} className="tagbox-span">
+                                    <a className="tagbox-name" target="_blank" href={base_url.tv9+data.authorTag} >
+                                        <span>{data.authorName}</span>
+                                    </a>
+                                    <a><AiFillCloseSquare className="cross" onClick={() => this.deleteTv9(data)}/></a>
+                                </span>
+                            ))}
+                            { this.props.tv6data.length>0 && (<h5 className="form-sub-heading">Tv6 Authors</h5>) }  
+                            {this.props.tv6data.map((data, idx) => (
+                                <span key={idx} className="tagbox-span">
+                                    <a className="tagbox-name" target="_blank" href={base_url.tv6+data.authorTag} >
+                                        <span>{data.authorName}</span>
+                                    </a>
+                                    <a><AiFillCloseSquare className="cross" onClick={() => this.deleteTv6(data)}/></a>
                                 </span>
                             ))}
                         </Col>
