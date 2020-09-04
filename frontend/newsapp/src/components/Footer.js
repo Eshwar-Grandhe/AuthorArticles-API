c01cf7938a51176f34a6bfb68490964c4e068d8a
Eshwar Grandhe <44287126+Eshwar-Grandhe@users.noreply.github.com>
2020-09-04 16:37:13 +0530

    Update Footer.js

--- frontend/newsapp/src/components/Footer.js
+++ frontend/newsapp/src/components/Footer.js
@@ -7,7 +7,7 @@ class Footer extends Component {
             <footer>
                 <div className="footer">
                     <h2 className="text-center" style={{marginBottom: 30}}>The Column</h2>
-                    <h5 className="text-center">Created by <a className="linked-link" href="https://www.linkedin.com/in/bharath-chandra-blotzmann/">Bharath</a> and <a className="linked-link" href="https://www.linkedin.com/in/eshwar-anirudh-782288178/">Eshwar</a></h5>
+                    <h5 className="text-center">Developed by <a className="linked-link" href="https://www.linkedin.com/in/bharath-chandra-blotzmann/">Bharath</a> and <a className="linked-link" href="https://www.linkedin.com/in/eshwar-anirudh-782288178/">Eshwar</a></h5>
                     <p className="text-center">&copy; The Column</p>
                 </div>
             </footer>
